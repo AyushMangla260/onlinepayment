12b7ea571183ae02f9f327ec37f4cd4d245a2b5e
AyushMangla260 <aayushmangla260@gmail.com>
2023-09-20 14:35:50 +0530

    Ayush commit

---
+++ onlinepaymentsystem/src/onlinepaymentsystem/PayPal.java
@@ -0,0 +1,8 @@
+package onlinepaymentsystem;
+
+public class PayPal {
+	 public static void main (String[] args)
+	 {
+		 System.out.println("This is payPal");
+	 }
+}
---
+++ onlinepaymentsystem/src/onlinepaymentsystem/UPIPayment.java
@@ -0,0 +1,8 @@
+package onlinepaymentsystem;
+
+public class UPIPayment {
+	 public static void main (String[] args)
+	 {
+		 System.out.println("This is UPI Payment");
+	 }
+}
